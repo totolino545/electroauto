f8f90d0a48e36ff48e668c264e0354dda6242d04
totolino545 <juanherrera545@hotmail.com>
2023-08-29 23:58:43 -0300

    first commit

---
+++ draw.js
@@ -0,0 +1,36 @@
+var DIR = "s/";
+
+function antiniebla() {
+  document.getElementById("diagrama").style.backgroundImage = 'url(imagenes/auxiliar.jpg)';
+}
+
+function giros(){
+  document.getElementById("diagrama").style.backgroundImage = 'url(imagenes/giros.jpg)';
+}
+
+function baliza(){
+  document.getElementById("diagrama").style.backgroundImage = 'url(imagenes/baliza.jpg)';
+}
+  
+function bulbo(){
+  document.getElementById("diagrama").style.backgroundImage = 'url(imagenes/bulbo-stop.jpg)';
+}
+  
+function h4(){
+  document.getElementById("diagrama").style.backgroundImage = 'url(imagenes/h4_luz_alta_baja.jpg)';
+}
+
+function levantavidrios(){
+  document.getElementById("diagrama").style.backgroundImage = 'url(imagenes/Levanta_vidrios.jpg)';
+}
+
+function bulboaatras(){
+  document.getElementById("diagrama").style.backgroundImage = 'url(imagenes/bulbo_marcha_atras_proc.jpg)';
+}
+
+function estereo(){
+  document.getElementById("diagrama").style.backgroundImage = 'url(imagenes/estereo_proc.jpg)';
+}
+
+
+
